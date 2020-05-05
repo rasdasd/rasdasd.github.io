1c4ac2225c547c0997891784d5d53c6d1d617c81
rasdasd <dave.hirschberg@gmail.com>
2020-05-04 22:03:08 -0700

    survivor guest who?

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,29 @@
 class Person {
     node = null;
-    constructor(id) {
+    constructor(id, player) {
         this.id = id;
         this.node = document.createElement('div');
         this.node.classList.add('person');
-        frame = document.createElement('img');
-        frame.src = '/assets/frame.png'
-        frame.classList.add('frame')
-        photo = document.createElement('img');
-        photo.src = `/assets/${id}`;
+        this.node.classList.add(`person-${player}`);
+        let photo = document.createElement('img');
+        photo.src = `assets/${id}.jpg`;
         photo.classList.add('photo')
-        this.node.append(frame);
-        frame.append(photo);
+        this.node.append(photo);
+        photo.onclick = () => this.toggle();
     }
+
+    reset() {
+        this.toggle(true);
+    }
+
+    toggle(forced) {
+        if (forced) {
+            this.node.classList.remove('face-down');
+        } else {
+            this.node.classList.toggle('face-down');
+        }
+    }
+
 }
 
 class Tray {
@@ -22,26 +33,33 @@ class Tray {
         this.node = document.createElement('div');
         this.node.classList.add('tray');
         this.node.classList.add(`tray-${player}`);
+        this.persons = [];
+        for (let i = 0; i < 24; i++) {
+            const person = new Person(i, player);
+            this.persons.push(person);
+            this.node.append(person.node);
+        }
     }
 
     reset() {
-
+        this.persons.forEach( p => p.reset());
     }
 }
 
-player1 = new Tray(1);
-player2 = new Tray(2);
+const player1 = new Tray(1);
+const player2 = new Tray(2);
 
 const root = document.getElementById('root');
 root.append(player1.node);
 root.append(player2.node);
 
 const resetButton = document.createElement('button');
-resetButton.onclick(() => {
+resetButton.textContent = 'Restart Game'
+resetButton.onclick = () => {
     player1.reset();
     player2.reset();
-});
+};
 resetButton.classList.add('reset-button');
 
-root.append(resetButton);
+document.getElementsByTagName('body')[0].append(resetButton);
 
